8e61791156d461fac3f70cbfeb5b65533cd140cf
Abhay <abhaykrishna280@gmail.com>
2025-09-02 22:38:14 +0530

    Working

---
+++ task1_image_metadata_analysis/Metadata.py
@@ -0,0 +1,25 @@
+import json
+import cv2
+import pytesseract
+import exiftool
+
+# --- CONFIG ---
+exiftool_path = r"C:\Users\Abhay\OneDrive\Desktop\HacKP-Tasks\task1_image_metadata_analysis\exiftool.exe"
+image_path = r"C:\Users\Abhay\OneDrive\Desktop\HacKP-Tasks\task1_image_metadata_analysis\dataset\4.jpg"
+
+# --- METADATA EXTRACTION ---
+with exiftool.ExifTool(exiftool_path) as et:
+    metadata_json = et.execute("-json", image_path)
+
+if metadata_json.strip():
+    try:
+        metadata = json.loads(metadata_json)[0]
+        print("\n=== METADATA INFO ===")
+        for key, value in metadata.items():
+            print(f"{key}: {value}")
+    except Exception as e:
+        print("Error parsing metadata:", e)
+        print("Raw output:", metadata_json)
+else:
+    print("\n=== METADATA INFO ===")
+    print("No metadata found for this image.")
